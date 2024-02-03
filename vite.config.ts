9c27f993266b60b619c620fc681241489bac0dff
Aaron Hammer <athammer@buffalo.edu>
2024-02-03 10:30:17 -0700

    cloudflare config

--- vite.config.ts
+++ vite.config.ts
@@ -3,9 +3,11 @@ import { defineConfig } from "@solidjs/start/config";
 export default defineConfig({
   start: {
     server: {
+      preset: "cloudflare-pages",
       prerender: {
         crawlLinks: true,
       },
     },
+    ssr: true,
   },
 });
