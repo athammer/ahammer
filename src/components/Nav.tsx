5b361fd8f5c8768e8845f48de4b6b21d73a9cc3f
Aaron Hammer <athammer@buffalo.edu>
2024-02-03 12:28:33 -0700

    fixing external  links

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -13,31 +13,35 @@ export default function Nav() {
     <nav class="mb-8">
       <ul class="container flex items-center pr-3 font-semibold">
         <li class={`border-b-2 ${active("/")} mr-1.5 sm:mr-6`}>
-          <Link href="/blog" defaultBlack>
+          <Link href="/" defaultBlack external>
             Home
           </Link>
         </li>
         |
         <li class={`border-b-2 ${active("/blog")} mx-1.5 sm:mx-6`}>
-          <Link href={randomBlog()} defaultBlack>
+          <Link href={randomBlog()} defaultBlack external>
             Random Blog
           </Link>
         </li>
         |
         <li class={`border-b-2 ${active("/about")} mx-1.5 sm:mx-6`}>
-          <Link href="https://twitter.com/athammer_" defaultBlack>
+          <Link href="https://twitter.com/athammer_" defaultBlack external>
             Twitter
           </Link>
         </li>
         |
         <li class={`border-b-2 ${active("/about")} mx-1.5 sm:mx-6`}>
-          <Link href="https://www.linkedin.com/in/athammer/" defaultBlack>
+          <Link
+            href="https://www.linkedin.com/in/athammer/"
+            defaultBlack
+            external
+          >
             Linkedin
           </Link>
         </li>
         |
         <li class={`border-b-2 ${active("/about")} mx-1.5 sm:mx-6`}>
-          <Link href="https://github.com/athammer" defaultBlack>
+          <Link href="https://github.com/athammer" defaultBlack external>
             Github
           </Link>
         </li>
--- src/routes/blog/fix-for-deploying-static-solid-start-5.x-to-cloudflare-pages.tsx
+++ src/routes/blog/fix-for-deploying-static-solid-start-5.x-to-cloudflare-pages.tsx
@@ -9,19 +9,32 @@ export default function DeployingSolidStartCloudflarePages() {
   return (
     <BlogPost title="Deploying your Statis Solid Start 5.x to Cloudflare Pages">
       <BlogParagraph>
-        <Link href="https://vinxi.vercel.app/">SolidStart</Link>, a
-        meta-framework for <Link href="https://www.solidjs.com/">SolidJS</Link>{" "}
+        <Link href="https://vinxi.vercel.app/" external>
+          SolidStart
+        </Link>
+        , a meta-framework for{" "}
+        <Link href="https://www.solidjs.com/" external>
+          SolidJS
+        </Link>{" "}
         recently went through a large shift in philosophy in it's latest 4.0
         release. They introduced{" "}
-        <Link href="https://vinxi.vercel.app/">Vinxi</Link>, an agnostic
-        Framework Bundler that leverages the power of{" "}
-        <Link href="https://vitest.dev/">Vite</Link> and{" "}
-        <Link href="https://nitro.unjs.io/">Nitro</Link>. With this change
-        SolidStart can focus more on the core framework and less on the bundler
-        and server. However this means the deploy processes has also shifted.
-        process has also shifted. This broke a lot of deployment processes for
-        people who were using the previous version of SolidStart. This blog post
-        will cover how to fix your SolidStart 5.x deploy to Cloudflare Pages.
+        <Link href="https://vinxi.vercel.app/" external>
+          Vinxi
+        </Link>
+        , an agnostic Framework Bundler that leverages the power of{" "}
+        <Link href="https://vitest.dev/" external>
+          Vite
+        </Link>{" "}
+        and{" "}
+        <Link href="https://nitro.unjs.io/" external>
+          Nitro
+        </Link>
+        . With this change SolidStart can focus more on the core framework and
+        less on the bundler and server. However this means the deploy processes
+        has also shifted. process has also shifted. This broke a lot of
+        deployment processes for people who were using the previous version of
+        SolidStart. This blog post will cover how to fix your SolidStart 5.x
+        deploy to Cloudflare Pages.
       </BlogParagraph>
 
       <BlogParagraph subtitle="Updating your Vite Config">
@@ -48,8 +61,10 @@ export default defineConfig({
       <BlogParagraph subtitle="Updating your Vite Config">
         And that's it! You should now be able to deploy your SolidStart 5.x to
         Cloudflare Pages. If you're still having issues, reach out in the{" "}
-        <Link href="https://discord.gg/solidjs">SolidStart's Discord</Link>.
-        Below is my build configuration as well in case it helps!
+        <Link href="https://discord.gg/solidjs" external>
+          SolidStart's Discord
+        </Link>
+        . Below is my build configuration as well in case it helps!
         <img
           src="/images/blog/deploy-solidstart-to-cloudflare/config.webp"
           alt="deploy config for cloudflare pages"
