3f1d32d11a4e00aef4836d370d25fc01aaedf9d9
Aaron Hammer <ahammer@Aarons-Laptop.local>
2024-05-05 20:09:09 -0600

    adding some text to blog and spacing

--- src/routes/blog/setting-up-square-payment-links-the-stripe-alternative.tsx
+++ src/routes/blog/setting-up-square-payment-links-the-stripe-alternative.tsx
@@ -1,10 +1,17 @@
 import { BlogPost } from "~/features/blog/ui/BlogPost";
 import { BlogParagraph } from "~/features/blog/ui/BlogParagraph";
+import { BlogParagraphSubsection } from "~/features/blog/ui/BlogParagraphSubsection";
 
 export default function SettingUpSquarePaymentLinks() {
   return (
     <BlogPost title="Setting up Square Payment Links the Stripe Alternative">
       <BlogParagraph>WIP :)</BlogParagraph>
+      <BlogParagraphSubsection>Square Payment Links?</BlogParagraphSubsection>
+      <BlogParagraph>
+        Previously known as Square Online Checkout, Square Payment Links allow you to take payment through a generic payment page much like Stripe Payment Links. While not as fleshed out as Stripe, Square offers world class integrations for any brick and mortar store such as resturatns, coffee shops, or any quick or full service restaurant while offering a much easier setup compared to Stripe if you're looking to get set up fast.
+      </BlogParagraph>
+      <BlogParagraphSubsection>Setting up Square Payment Links</BlogParagraphSubsection>
+      <BlogParagraph></BlogParagraph>
     </BlogPost>
   );
 }
