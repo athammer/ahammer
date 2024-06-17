0200e0958f6fdf3a9eeda17d5d45118152a3c178
Aaron Hammer <ahammer@Aarons-Laptop.local>
2024-06-16 19:51:47 -0600

    fixing deploy

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,13 @@
 import { defineConfig } from "@solidjs/start/config";
 
+// https://ryanjc.com/blog/solidstart-cloudflare-pages/
 export default defineConfig({
-  start: {
-    server: {
-      preset: "cloudflare-pages",
+  server: {
+    preset: "cloudflare-pages",
+    // We will need to enable CF Pages node compatiblity
+    // https://developers.cloudflare.com/workers/runtime-apis/nodejs/asynclocalstorage/
+    rollupConfig: {
+      external: ["__STATIC_CONTENT_MANIFEST", "node:async_hooks"],
     },
   },
-});
+})
